9014105699783c9f31b0ab0276e55cbc5df50841
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-14 13:27:01 +0900

    3回分の小テストの合計を求めるプログラムSample6_1_1.javaを作成しました

---
+++ src/main/java/Sample/Sample6_1_1.java
@@ -0,0 +1,11 @@
+public class Sample6_1_1 {
+    public static void main(String[] args) {
+        int score1 = 70;
+        int score2 = 80;
+        int score3 = 90;
+
+        int total = score1 + score2 + score3;
+
+        System.out.println(total);
+    }
+}
