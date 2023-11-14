73563617a8870e6df1c60943686bdf5f5e73f19c
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-11-14 15:09:26 +0900

    プログラム例　3回分の小テストの合計を求めるプログラムSample6_3_1.javaを作成しました

---
+++ src/main/java/Sample/Sample6_3_1.java
@@ -0,0 +1,9 @@
+public class Sample6_3_1 {
+    public static void main(String[] args) {
+        int[] arrayScore = {80, 70, 90};        //配列の初期化
+        int total;                              //累計エリア
+
+        total = arrayScore[0] + arrayScore[1] + arrayScore[2];
+        System.out.println(total);
+    }
+}
